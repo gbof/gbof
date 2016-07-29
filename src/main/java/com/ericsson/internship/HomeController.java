3f0a7c1506c3855e7f6b68976afc2ebef2f18c1d
p756 <p756@pc-ldt1-10-106.ericpol.int>
2016-07-29 09:55:33 +0200

    2.1 commit

--- src/main/java/com/ericsson/internship/HomeController.java
+++ src/main/java/com/ericsson/internship/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -26,14 +27,13 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public ModelAndView indexPage() {
+		return new ModelAndView("home");
 	}
 	
 }
